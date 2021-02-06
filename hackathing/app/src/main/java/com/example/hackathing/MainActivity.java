20fdb0156e9abdfbfb9ede5ebe088805e2367609
XiaoMengLii <xli422@uottawa.ca>
2021-02-06 00:02:31 -0500

    google maps function added

--- hackathing/app/src/main/java/com/example/hackathing/MainActivity.java
+++ hackathing/app/src/main/java/com/example/hackathing/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.hackathing;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+
+    public void OnOpenInGoogleMaps (View view) {
+        EditText teamAddres = (EditText) findViewById(R.id.addr);
+
+        Uri gmmIntentUri = Uri.parse("http://maps.google.co.in/maps?q="+teamAddres.getText());
+
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+
+        mapIntent.setPackage("com.google.android.apps.maps");
+
+        startActivity(mapIntent);
+    }
+
+
 }
