9bf3a968080218cf5f2fa9f677f75dca881e4eae
XiaoMengLii <xli422@uottawa.ca>
2021-02-06 22:05:00 -0500

    implemented the search button and updated google maps's search address

--- hackathing/app/src/main/java/com/example/hackathing/MainActivity.java
+++ hackathing/app/src/main/java/com/example/hackathing/MainActivity.java
@@ -15,6 +15,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.util.Log;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -28,6 +29,7 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
 
@@ -42,9 +44,13 @@ public class MainActivity extends AppCompatActivity {
     static double hosLat;
     static double lowestV;
     static double [] x = new double[148];
+    static double [] xcl = new double[148];
+    static int [] finalind = new int [10];
     static int r = 6371;
     static int lowest;
     static String nameof;
+    static String addre;
+    static int [] ind = new int[148];
 
     ListView clinicInfoList;
     ArrayList<String> clinicInfo;
@@ -79,20 +85,9 @@ public class MainActivity extends AppCompatActivity {
         statsList.setAdapter(adapterStats);
     }
 
-    public void OnOpenInGoogleMaps (View view) {
+    public void Search (View view){
         EditText teamAddres = (EditText) findViewById(R.id.addr);
-
-//        String locationName = teamAddres + ", " + "Canada";
-//        Geocoder geoCoder = new Geocoder(getBaseContext(), Locale.getDefault());
-//        try {
-//            List<Address> address = geoCoder.getFromLocationName(locationName, 1);
-//           latitude = address.get(0).getLatitude();
-//           longitude = address.get(0).getLongitude();
-//           Log.d("simon", String.valueOf(latitude));
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//            Log.d("simon",e.toString());
-//        }
+        TextView textView12 = (TextView) findViewById(R.id.textView12);
 
         Geocoder geoCoder = new Geocoder(this);
         String a = teamAddres.getText().toString();
@@ -115,49 +110,7 @@ public class MainActivity extends AppCompatActivity {
                 // Display appropriate message when Geocoder services are not available
                 Toast.makeText(this, "Unable to geocode zipcode", Toast.LENGTH_LONG).show();
             }
-        } catch (IOException e) {
-            // handle exception
-            Log.d("simon",e.toString());
-        }
 
-        Uri gmmIntentUri = Uri.parse("http://maps.google.co.in/maps?q="+teamAddres.getText());
-
-        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
-
-        mapIntent.setPackage("com.google.android.apps.maps");
-
-        startActivity(mapIntent);
-    }
-
-    //Method for making the initial API call to fetch all of the test centers
-    //Info stored in testCenters object in Main Activity
-    private static class testCenterRequest extends AsyncTask<String, Void, String> {
-        @Override
-        protected String doInBackground(String... params) {
-            String url = "https://services1.arcgis.com/B6yKvIZqzuOr0jBR/arcgis/rest/services/COVID19_Testing_Centres_in_Canada/FeatureServer/0/query?where=1=1&outFields=*&outSR=4326&f=json";
-            String output;
-            RequestFuture<JSONObject> future = RequestFuture.newFuture();
-
-            // Request a string response from the provided URL.
-            JsonObjectRequest stringRequest = new JsonObjectRequest(Request.Method.GET, url, null, future, future);
-            // Add the request to the RequestQueue.
-            MainActivity.requestQueue.add(stringRequest);
-
-            try {
-                JSONObject result = future.get();
-                MainActivity.testCenters = new TestCenters((JSONArray) result.get("features"));
-                Log.d("MyTag", String.valueOf(MainActivity.testCenters.numberOfTestCenters()));
-                output = "true";
-            } catch (ExecutionException | JSONException | InterruptedException e) {
-                e.printStackTrace();
-                output = "false";
-            }
-            return output;
-        }
-
-        @Override
-        protected void onPostExecute(String result) {
-            //Put everything to do with accessing TestCenters here
             JSONObject jsonOb;
             JSONObject jsonOb2;
             try {
@@ -207,10 +160,68 @@ public class MainActivity extends AppCompatActivity {
             try {
                 jsonOb2 = MainActivity.testCenters.getName(lowest);
                 nameof = jsonOb2.getString("USER_Name");
+                addre = jsonOb2.getString("USER_Street");
             }catch(JSONException e) {
                 e.printStackTrace();
             }
             Log.d("hiiiii", nameof);
+        } catch (IOException e) {
+            // handle exception
+            Log.d("simon",e.toString());
+        }
+        textView12.setText(nameof);
+
+
+
+    }
+
+    public void Next (View view){
+
+    }
+
+    public void OnOpenInGoogleMaps (View view) {
+
+
+        Uri gmmIntentUri = Uri.parse("http://maps.google.co.in/maps?q="+addre);
+
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+
+        mapIntent.setPackage("com.google.android.apps.maps");
+
+        startActivity(mapIntent);
+    }
+
+    //Method for making the initial API call to fetch all of the test centers
+    //Info stored in testCenters object in Main Activity
+    private static class testCenterRequest extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... params) {
+            String url = "https://services1.arcgis.com/B6yKvIZqzuOr0jBR/arcgis/rest/services/COVID19_Testing_Centres_in_Canada/FeatureServer/0/query?where=1=1&outFields=*&outSR=4326&f=json";
+            String output;
+            RequestFuture<JSONObject> future = RequestFuture.newFuture();
+
+            // Request a string response from the provided URL.
+            JsonObjectRequest stringRequest = new JsonObjectRequest(Request.Method.GET, url, null, future, future);
+            // Add the request to the RequestQueue.
+            MainActivity.requestQueue.add(stringRequest);
+
+            try {
+                JSONObject result = future.get();
+                MainActivity.testCenters = new TestCenters((JSONArray) result.get("features"));
+                Log.d("MyTag", String.valueOf(MainActivity.testCenters.numberOfTestCenters()));
+                output = "true";
+            } catch (ExecutionException | JSONException | InterruptedException e) {
+                e.printStackTrace();
+                output = "false";
+            }
+            return output;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            //Put everything to do with accessing TestCenters here
+
+
         }
     }
 }
