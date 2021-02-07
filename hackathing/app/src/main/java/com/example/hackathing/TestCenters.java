bb6d68a13969eacb6bbbc42b176300942917030d
Bill729 <s11050003@gmail.com>
2021-02-06 22:47:52 -0500

    added getter methods for address,hours of op., names distance and the indexes for the top 10 closest hospitals are stored as a global array.

--- hackathing/app/src/main/java/com/example/hackathing/TestCenters.java
+++ hackathing/app/src/main/java/com/example/hackathing/TestCenters.java
@@ -25,13 +25,64 @@ public class TestCenters {
         }
         return  coordinateJson;
     }
-    public JSONObject getName(int number) {
+    public String getName(int number) {
         JSONObject nameJson;
+        String names;
         try {
             nameJson = (JSONObject) ((JSONObject) locations.get(number)).get("attributes");
+            names = nameJson.getString("USER_Name");
         } catch (JSONException e) {
             nameJson = new JSONObject();
+            names = new String();
         }
-        return  nameJson;
+        return  names;
     }
+    public String getAddress(int number) {
+        JSONObject addressJson;
+        String st;
+        String city;
+        String prov;
+        String pos;
+        String address="";
+        try {
+            addressJson = (JSONObject) ((JSONObject) locations.get(number)).get("attributes");
+            st = addressJson.getString("USER_Street");
+            city = addressJson.getString("USER_City");
+            prov = addressJson.getString("USER_Prov");
+            pos = addressJson.getString("USER_PostalCode");
+            address = st+", "+city+", "+prov+", "+pos;
+        } catch (JSONException e) {
+            addressJson = new JSONObject();
+            address = new String();
+        }
+        return  address;
+    }
+    public String getHOP(int number) {
+        JSONObject hopJson;
+        String mon;
+        String tue;
+        String wed;
+        String thu;
+        String fri;
+        String sat;
+        String sun;
+        String hop="";
+        try {
+            hopJson = (JSONObject) ((JSONObject) locations.get(number)).get("attributes");
+            mon = hopJson.getString("USER_Mon");
+            tue = hopJson.getString("USER_Tue");
+            wed = hopJson.getString("USER_Wed");
+            thu = hopJson.getString("USER_Thur");
+            fri= hopJson.getString("USER_Fri");
+            sat = hopJson.getString("USER_Sat");
+            sun = hopJson.getString("USER_Sun");
+            hop = "Mon: "+mon+", "+"Tue: "+tue+", "+"Wed: "+wed+", "+"Thur: "+thu+", "+"Fri: "+fri+", "+"Sat: "+sat+", "+"Sun: "+sun;
+        } catch (JSONException e) {
+            hopJson = new JSONObject();
+            hop = new String();
+        }
+        return  hop;
+    }
+
+
 }
